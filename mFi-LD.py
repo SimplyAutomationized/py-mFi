5899fcd59b0debb435d3a25522d59d285b2b399b
Nathan Rees <Nextabyte@gmail.com>
2016-01-14 20:44:03 -0800

    refactoring

--- mFi-LD.py
+++ mFi-LD.py
@@ -1,11 +1,9 @@
 import json
 from mPower import *
 import threading
-from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory,connectWS
+from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory, connectWS
 from time import sleep
 from twisted.internet import reactor
-
-
 
 from twisted.internet.protocol import ReconnectingClientFactory
 
@@ -14,20 +12,20 @@ from autobahn.twisted.websocket import WebSocketClientProtocol, \
 
 mFI = None
 
+
 class mFiWebSocketClient(WebSocketClientFactory, ReconnectingClientFactory):
-
     class mFiWebSocketClientProtocol(WebSocketClientProtocol):
-            def onOpen(self):
-                self.sendMessage('{"time":10}')
-                self.factory.send_cmd = self.send_cmd
+        def onOpen(self):
+            self.sendMessage('{"time":10}')
+            self.factory.send_cmd = self.send_cmd
 
-            def onMessage(self, payload, isBinary):
-                self.factory.recv_data(payload, isBinary)
+        def onMessage(self, payload, isBinary):
+            self.factory.recv_data(payload, isBinary)
 
-            def send_cmd(self, data):
-                print data
-                #{"sensors":[{"output":1,"port":1}]}
-                self.sendMessage(json.dumps(data))
+        def send_cmd(self, data):
+            print data
+            # {"sensors":[{"output":1,"port":1}]}
+            self.sendMessage(json.dumps(data))
 
     protocol = mFiWebSocketClientProtocol
     status = {}
@@ -53,13 +51,11 @@ class mSwitch(mPower, mFiWebSocketClient):
 
     def __init__(self, ip, username, password, datachangeCallback=None, port=7682):
         mPower.__init__(self)
-        #7682
         mFiWebSocketClient.__init__(self, "wss://{}:{}/?username={}&password={}".format(ip, port, username, password),
                                     protocols=['mfi-protocol'])
         self.output
         self.dimmer_level
         self.callback = datachangeCallback
-
 
     @property
     def dimmer_level(self):
@@ -72,7 +68,7 @@ class mSwitch(mPower, mFiWebSocketClient):
     @dimmer_level.setter
     def dimmer_level(self, value):
         self._dimmer_level = value
-        data = {"sensors": [{"dimmer_level":value, "port":1}]}
+        data = {"sensors": [{"dimmer_level": value, "port": 1}]}
         self.send_cmd(data)
 
     @property
@@ -82,7 +78,7 @@ class mSwitch(mPower, mFiWebSocketClient):
     @output.setter
     def output(self, value):
         self._output = value
-        data = {"sensors": [{"output":value,"port": 1}]}
+        data = {"sensors": [{"output": value, "port": 1}]}
         self.send_cmd(json.dumps(data))
 
     def recv_data(self, payload, isBinary):
@@ -90,24 +86,27 @@ class mSwitch(mPower, mFiWebSocketClient):
             print payload
             self.status = json.loads(payload)['sensors'][0]
             for key in self.status.keys():
-                setattr(self, '_'+key, self.status[key])
+                setattr(self, '_' + key, self.status[key])
             if self.callback is not None:
                 self.callback(self.status)
         else:
             pass
 
+
 def callback(data):
     print mFI.dimmer_level
 
+
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('address', help="address", default="localhost", nargs="?")
     parser.add_argument('port', help="port", default=7682, nargs="?")
-    parser.add_argument('username',help='username',default='ubnt',nargs="?")
-    parser.add_argument('pwd',help='password',default='ubnt',nargs="?")
+    parser.add_argument('username', help='username', default='ubnt', nargs="?")
+    parser.add_argument('pwd', help='password', default='ubnt', nargs="?")
     args = parser.parse_args()
 
-    mFI = mSwitch(args.address,args.username,args.pwd, callback, port=args.port)
+    mFI = mSwitch(args.address, args.username, args.pwd, callback, port=args.port)
     connectWS(mFI)
     reactor.run()
