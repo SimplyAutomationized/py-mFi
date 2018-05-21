634c1b7c1cebec1d8723d38c8b7f6f9a5f636ff3
Kevron Rees <tripzero.kev@gmail.com>
2018-05-21 11:28:16 -0700

    mpower script python3 support
    Signed-off-by: Kevron Rees <tripzero.kev@gmail.com>

---
+++ mfi/resource_py2.py
@@ -0,0 +1,37 @@
+import trollius as asyncio
+
+
+class Py2Resource:
+
+    def __init__(self, poll_func, sleep_timer):
+        self.poll_func = poll_func
+        self.sleep_timer = sleep_timer
+
+        asyncio.get_event_loop().create_task(self.poll())
+
+
+    @asyncio.coroutine
+    def poll(self):
+        while True:
+            self.poll_func()
+            yield asyncio.From(asyncio.sleep(self.sleep_timer))
+
+
+
+class Py2PollWhileTrue:
+
+    def __init__(self, poll_func, sleep_timer):
+        self.poll_func = poll_func
+        self.sleep_timer = sleep_timer
+
+        asyncio.get_event_loop().create_task(self.poll())
+
+
+    @asyncio.coroutine
+    def poll(self):
+        if callable(self.poll_func):
+            while self.poll_func():
+                yield asyncio.From(asyncio.sleep(self.sleep_timer))
+        else:
+            while self.poll_func:
+                yield asyncio.From(asyncio.sleep(self.sleep_timer))
