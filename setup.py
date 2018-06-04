935e5968f966a2d38f30beab28dab752f75f1c1e
Kevron Rees <tripzero.kev@gmail.com>
2018-06-04 13:01:36 -0700

    make setup.py python2 and 3 compatible. version bump 1.1

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@
 
 from setuptools import setup, Extension
 
+install_requires = ["twisted", "wss", "PySignal"]
+
+import sys
+if sys.version_info < (3,0):
+    install_requires.append("trollius")
+
 setup(name='mfi',
-      version='1.0',
+      version='1.1',
       description='Python library for interacting with various ubiquity mfi devices',
       author='Kevron Rees, Nathan Rees',
       author_email='tripzero.kev@gmail.com, nextabyte@gmail.com',
       url='https://github.com/SimplyAutomationized/py-mFi',
       packages=["mfi"],
       scripts=['mfi/mpower'], 
-      install_requires=["trollius", "twisted",  "wss",  "PySignal"]
+      install_requires=install_requires
       )
